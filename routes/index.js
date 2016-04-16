cde243f03f0f2a0c68604f510be0fe9949b7f181
lynliyan <lynliyan@192.168.3.9>
2016-04-16 22:18:45 +0800

    useradd

--- routes/index.js
+++ routes/index.js
@@ -16,12 +16,14 @@ router.get('/app', function(req, res, next) {
 router.get('/welogin', function(req, res, next){
 	wechat.getAccessToken(req.query.code, function(err, result){
 		var refresh = result.refresh_token;
+		console.log(err);
 		wechat.refreshAccessToken(refresh, function(err, result){
+			console.log(err);
 			wechat.getUserInfo(result.access_token, result.openid, function(err, result){
+				console.log(result);
 				res.redirect("/home.html#&pageMeEdit");
 			});					
-		});
-
+		})
 		
 	});
 
