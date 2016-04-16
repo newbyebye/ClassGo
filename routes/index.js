eb1f259cc9df8c6a91ed5a8813a3337e6d4b2ec4
lynliyan <lynliyan@192.168.3.9>
2016-04-16 22:37:52 +0800

    add token

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,10 @@ router.get('/welogin', function(req, res, next){
 	wechat.getAccessToken(req.query.code, function(err, result){
 		console.log(result);
 		var refresh = result.refresh_token;
-
+		console.log("refresh token:" + refresh);
 		wechat.refreshAccessToken(refresh, function(err, result){
 			console.log(result);
-			
+
 			wechat.getUserInfo(result.access_token, result.openid, function(err, result){
 				console.log(result);
 				res.redirect("/home.html#&pageMeEdit");
