11769d4a5114db870d69bd4bbdc19a403005cce4
lynliyan <lynliyan@192.168.3.9>
2016-04-24 11:07:21 +0800

    enable gzip

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var compression = require('compression');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -33,6 +34,7 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
